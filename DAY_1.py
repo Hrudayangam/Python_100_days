c545d9ea0e67c2e314dc3bf2a94c2f44e44796fc
Hrudayangam Mehta <51739491+Hrudayangam@users.noreply.github.com>
2021-05-30 06:43:39 +0530

    Add files via upload
    print, input, comment, variables, string manipulation.

---
+++ DAY_1.py
@@ -0,0 +1,26 @@
+##DAY 1 
+
+#PRINT
+
+print("day 1")
+
+print("Hello world")
+print("ok \n ok ")
+print("playing with strings")
+#-------------------------------------------------------------------------------------------------------#
+
+##INPUT 
+x = input("enter your name ")
+
+print(len(x))
+
+j=0 
+for i in x:
+    j = j + 1
+print("lenth of word is: " ,j)                
+#-------------------------------------------------------------------------------------------------------#
+
+print(" welcome to brand name generator \n")
+x =  ( input(" which city did you grow up in? \n"))
+y =  ( input (" whats the name of your pet? \n"))
+print ("your brand name is :", x + y )
